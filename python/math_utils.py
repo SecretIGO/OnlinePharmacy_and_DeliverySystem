6199a5c4e84196ae1adb62177e225cc79e6450f7
SecretIGO <110442934+SecretIGO@users.noreply.github.com>
2023-07-02 18:43:59 +0800

    left behind

--- python/math_utils.py
+++ python/math_utils.py
@@ -1,6 +1,11 @@
 # library for calculations
 
-def item_total(item, qty):
-    return item * qty
+def calculate_itemTotal(item_price, qty):
+    return item_price * qty
+
+def calculate_itemRating(item_rating1s, item_rating2s, item_rating3s, item_rating4s, item_rating5s, total_numOf_ratings):
+    return round(((1*item_rating1s)+(2*item_rating2s)+(3*item_rating3s)+(4*item_rating4s)+(5*item_rating5s))/float(total_numOf_ratings), 2)
+
+
     
 
